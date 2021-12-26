9b6f9a5b4178373774e420ca68976a3924f1d3a2
Lincoln Monteiro <lincolngondim@gmail.com>
2021-12-26 17:48:16 -0300

    Updated

--- index.js
+++ index.js
@@ -1,44 +1,51 @@
 const board = document.getElementById('draw_board');
-const color_picker = document.getElementById('color_hex');
-const image_name = document.getElementById('image_name');
+const color_picker = document.getElementById('color_input');
+const image_name   = document.getElementById('name_input');
+const htmltext     = document.getElementById('html-text');
+const csstext      = document.getElementById('css-text');
+const colorviewer  = document.getElementById('selectedcolor-viewer');
+
+//change canvas draw size
+const size   = {width: 10, height:10};
+board.width  = size.width; 
+board.height = size.height;
+
+//context
+const context   = board.getContext('2d');
+const imageData = context.createImageData(size.width, size.height);
+
+//all box shadows
+let boxes = '';
+//all colors of the board in hexadecimal
 const colors = [];
-let name = '';
+//name of class of the div
+let name = image_name.getAttribute('value');
+let selectedColor = '#000';
 
-image_name.onkeyup = (e)=>{
-	name = e.target.value;
-	document.querySelector('.html_text').innerText = '<div class="' + name + '"></div>';
-}
+//update the html viewer text 
+const updateHTMLViewer = classname => htmltext.innerText = '<div class="' + classname + '"></div>';
 
-let selectedColor = 'black';
-color_picker.onkeyup = (e)=>{
-	console.log(e.target.value);
-	selectedColor = (e.target.value[0]==='#')?e.target.value:'#'+e.target.value;
-	document.querySelector('.color_show').style.background = selectedColor;
-}
-const size = {width: 10, height:10};
-board.width=size.width; 
-board.height=size.height;
+const updateColorViewer = color => colorviewer.style.background = color;
 
-const context = board.getContext('2d');
-const data = context.createImageData(size.width, size.height);
+const updateCSSViewer = string => csstext.innerText = string;
 
-const generateCSS = ()=>{
+const getCSS = unity => `.${name}{\n width:1${unity};\n height:1${unity};\n box-shadow:\n  ${boxes}}`;
+
+const updateBox = ()=>{
 	let ref = [0, 0];
 	let unity = 'em'
-	let string = `.${name}{\n\twidth:1${unity};\n\theight:1${unity};\n\tbox-shadow:\n\t`;
-	let i = 0;
+	boxes = '';
 	for(let y = 0; y < size.height; y++){
 		for(let x = 0; x < size.width; x++){
 			if(colors[y*size.width + x]){
-				string += `${x-ref[0]}${unity} ${y-ref[0]}${unity} ${colors[y*size.width + x]},\n\t`
+				boxes += `${x-ref[0]}${unity} ${y-ref[0]}${unity} ${colors[y*size.width + x]},\n  `
 			}
-			i+=1;
 		}
 	}
-	string+='\n}'
-	return string;
+	boxes +='\n'
 }
 
+//convert a hexadecimal code value to an rgb format
 const toRGB = (hex)=>{
 	let h = hex.replace(' ', '').replace('#','');
 	const rgb = [0, 0, 0];
@@ -55,30 +62,46 @@ const toRGB = (hex)=>{
 	return rgb;
 }
 
-const intToHex = (v)=>{
-	let temp = v.toString(16)
-	return temp.length===1?'0'+temp:temp;
-}
+//convert an decimal value to kexadecimal value
+const intToHex = (v)=>{ let temp = v.toString(16); return temp.length===1?'0'+temp:temp;}
 
-const toHex = (r, g, b)=>{
-	return `#${intToHex(r)}${intToHex(g)}${intToHex(b)}`;
-}
+//return an hexadecimal color code from an rgb color
+const toHex = (r, g, b)=>{return `#${intToHex(r)}${intToHex(g)}${intToHex(b)}`;}
 
+//change an specific pixel value in canvas
 const putPixel = (x, y, r, g, b, a)=>{
-	const pos = ((y*4) * size.width) + (x*4);
-	data.data[pos] = r;
-	data.data[pos+1] = g;
-	data.data[pos+2] = b;
-	data.data[pos+3] = a;
+	const positionInArray = ((y*4) * size.width) + (x*4);
+	imageData.data[positionInArray + 0] = r;
+	imageData.data[positionInArray + 1] = g;
+	imageData.data[positionInArray + 2] = b;
+	imageData.data[positionInArray + 3] = a;
+}
+
+updateHTMLViewer(name);
+updateCSSViewer(getCSS('em'))
+
+
+//change selected color
+color_picker.onkeyup = (e)=>{
+	selectedColor = (e.target.value[0]==='#')?e.target.value:'#'+e.target.value;
+	updateColorViewer(selectedColor);
+}
+
+image_name.onkeyup = (e)=>{
+	name = e.target.value;
+	updateHTMLViewer(name);
+	updateCSSViewer(getCSS('em'))
 }
 
 board.onmousedown = (e)=>{
+	//take coordinates in the canvas
 	let x = Math.floor((e.layerX/e.target.clientWidth) * size.width);
-	let y =  Math.floor((e.layerY/e.target.clientHeight) * size.height);
+	let y = Math.floor((e.layerY/e.target.clientHeight) * size.height);
+	//convert the selected color in a rgb form
 	const cor = toRGB(selectedColor);
 	colors[y*size.width + x] = selectedColor;
 	putPixel(x, y, cor[0], cor[1], cor[2], 255);
-	const css = generateCSS();
-	document.querySelector('.css_text').innerText = css;
-	context.putImageData(data, 0, 0);
+	updateBox();
+	updateCSSViewer(getCSS('em'))
+	context.putImageData(imageData, 0, 0);
 }
